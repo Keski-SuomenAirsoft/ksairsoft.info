98825250fc559d78117454980dc061b587fd1c1b
Brage Sekse Aarset <brage@bjerk.io>
2022-03-26 01:15:27 +0200

    fix: disable image optimization

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const withMDX = require("@next/mdx")({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // disable image optimisation, since the default loader doesn't work with next export (and we are using theme UI images anyway)
+  images: {
+    loader: "custom",
+  },
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 };
 
