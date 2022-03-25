eca5b28a2c7dec02b420c32e42255263390575bb
Brage Sekse Aarset <brage@bjerk.io>
2022-03-26 00:53:56 +0200

    fix: config static CDN prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/braaar/braaar.github.io/ksairsoft.info/"
+    : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
