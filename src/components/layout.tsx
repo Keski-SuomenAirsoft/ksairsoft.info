bf32dc428a30c4443e3a4d40a733b4ee93534e94
Brage <brage.aarset@gmail.com>
2022-03-26 15:47:02 +0200

    feat: add social links (#5)
    * feat: add social links
    
    * fix: use icons instead of ugly banners
    
    * fix: remove social banner images
    
    * feat: add kuulaportti icon

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,6 +4,7 @@ import { Footer } from "./footer";
 import Head from "next/head";
 import { NavBar } from "./nav-bar";
 import { SponsorBox } from "./sponsor-box";
+import { Socials } from "./socials";
 
 export const Layout: React.FC = ({ children }) => {
   return (
@@ -18,9 +19,10 @@ export const Layout: React.FC = ({ children }) => {
         </Box>
         <Container sx={{ flex: "1 1 auto" }}>
           <Flex sx={{ flexDirection: ["column", "row"], gap: 3 }}>
-            <Box sx={{ minWidth: "15rem" }}>
+            <Flex sx={{ minWidth: "15rem", flexDirection: "column", gap: 3 }}>
               <NavBar />
-            </Box>
+              <Socials />
+            </Flex>
             <Box sx={{ flex: 2 }}>{children}</Box>
             <Box sx={{ minWidth: "15rem", flex: 1 }}>
               <SponsorBox />
