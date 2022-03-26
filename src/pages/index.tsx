5c539c75b4f442cb712de1d7f772a540d47e700e
Brage Sekse Aarset <brage@bjerk.io>
2022-03-26 14:06:14 +0200

    docs: add todo to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { allPages } from "contentlayer/generated";
 import { PageBox } from "src/components/page-box";
 import { Box } from "theme-ui";
 
+// TODO: Set up getStaticProps to optimise build
+
 const Home = () => {
   const homePage = allPages.find((post) => post.slug === "");
   if (homePage) return <PageBox post={homePage} />;
