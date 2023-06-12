d656ff1025e65385130edaca16ceb34bc0e70d7d
Keski-Suomen Airsoft Ry <111794792+Keski-SuomenAirsoft@users.noreply.github.com>
2023-06-12 09:44:17 +0300

    Update sponsor-box.tsx
    Päivitetty yhteistyökumppanien linkit

--- src/components/sponsor-box.tsx
+++ src/components/sponsor-box.tsx
@@ -13,24 +13,11 @@ export const SponsorBox: React.FC = () => {
       </Box>
       <Box sx={{ p: 2, maxWidth: "100%" }}>
         <Flex sx={{ flexDirection: "column", gap: 1 }}>
-          <Link href="https://armoria.fi/" target={"_blank"}>
-            <Image src={"/sponsors/armoria.jpeg"} alt="Armoria banner" />
-          </Link>
-          <Link href="https://milgear.fi/" target={"_blank"}>
-            <Image src={"/sponsors/milgear.jpeg"} alt="Milgear banner" />
-          </Link>
-          <Link href="https://hatka.fi/" target={"_blank"}>
-            <Image src={"/sponsors/hatka.jpeg"} alt="Hatka banner" />
-          </Link>
-          <Link href="https://atom-airsoft.fi/" target={"_blank"}>
-            <Image
-              src={"/sponsors/atomairsoft.jpeg"}
-              alt="Atom airsoft banner"
-            />
-          </Link>
+          
           <Link href="https://tierone.fi/" target={"_blank"}>
             <Image src={"/sponsors/tierone.png"} alt="Tierone banner" />
           </Link>
+          
         </Flex>
       </Box>
     </Flex>
